81bc06b1ba54276d0027777f6f7c63d93b447558
Mike Wang <diend1201@gmail.com>
2022-06-18 18:19:17 +0800

    add header

--- products.py
+++ products.py
@@ -4,8 +4,14 @@ while True:
 	if name == 'q': #quit
 		break # 有輸入商品才需問價格/q即可離開
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f: # w寫入/encoding(編碼;建議UTF-8)
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
