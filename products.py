fcaf1db1dca98c84482b7f9fa259d92dfc082998
Mike Wang <diend1201@gmail.com>
2022-06-18 17:54:34 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -4,12 +4,8 @@ while True:
 	if name == 'q': #quit
 		break # 有輸入商品才需問價格/q即可離開
 	price = input('請輸入商品價格: ')
-	# p = []
-	# p.append(name) # 建立小清單(小火車)
-	# p.append(price)
-	# p = [name, price] (7-9行可以精簡成一行)
-	# products.append(p) # 裝進大清單(大火車)
-	products.append([name, price]) # 直接(10-11行)更精簡!!
+	products.append([name, price])
 print(products)
 
-products[0][0] # 左0大為清單中的第一個/右0為小清單的第一個
+for p in products:
+	print(p[0], '的價格是', p[1])
